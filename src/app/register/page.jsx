2be81a1e2503abea94dfffd8b985b9ceada205db
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-05-14 17:26:25 +0600

    register page done

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -3,26 +3,30 @@ import Link from 'next/link';
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
 
-const LoginPage = () => {
-    const handleLogin = (e) =>{
+const RegistrationPage = () => {
+    const handleRegister = (e) =>{
         e.preventDefault();
-    };
+    }
     const handleGoogleLogin = () =>{
 
     }
     return (
-        <div className='min-h-[80vh] flex items-center justify-center p-4'>
-            <form onSubmit={handleLogin} className='fieldset w-full max-w-sm bg-base-200 border-base-200 rounded-box  border p-8 shadow-lg'>
+         <div className='min-h-[80vh] flex items-center justify-center p-4'>
+            <form onSubmit={handleRegister} className='fieldset bg-base-200 border-base-200 w-full max-w-sm rounded-box  border p-8 shadow-lg'>
                 <h1 className='text-2xl font-bold text-center mb-4 text-primary'>
-                    Login to SkillSphere
+                    Join SkillSphere
                 </h1>
+                <label className="fieldset-legend">Full Name</label>
+                <input type="text" className="input w-full" placeholder="Your Name" required />
                 <label className="fieldset-legend">Email</label>
                 <input type="email" className="input w-full" placeholder="Email" required />
+                <label className="fieldset-legend mt-4">Photo URL</label>
+                <input type="url" className="input w-full" placeholder="https://example.com/photo.jpg" required />
 
                 <label className="fieldset-legend mt-4">Password</label>
                 <input type="password" className="input w-full" placeholder="Password" required />
 
-                <button className="btn btn-primary mt-6">Login</button>
+                <button className="btn btn-primary mt-6">Register</button>
 
                 <div className="divider">OR</div>
                 <button 
@@ -31,19 +35,18 @@ const LoginPage = () => {
                     className="btn btn-outline flex items-center gap-2"
                 >
                     <FcGoogle className="text-xl" />
-                    Login with Google
+                    Register with Google
                 </button>
                 <p className="text-center mt-4 text-sm">
-                    Do not have an account?{" "}
-                    <Link href="/register" className="link link-primary font-semibold">
-                        Register here
+                    Already have an account?{" "}
+                    <Link href="/login" className="link link-primary font-semibold">
+                        Login here
                     </Link>
                 </p>
 
             </form>
         </div>
-       
     );
 };
 
-export default LoginPage;
+export default RegistrationPage;
